19be3646d148d64cdd7d06e9c56c1ec027499147
https://github.com/MikhailV89 <mvasilchenko89@gmail.com>
2024-12-27 11:15:41 +0200

    hw2 ready

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
-const arr = [3, 6, 2, -54, 4637, -2334, 0, 22];
+const arr = [3, 6, 2, -54, 4637, -2334, 0, 22,];
 let min = arr[0];
-for (let i = 0; i < arr.length - 1; i++) {
+for (let i = 0; i < arr.length; i++) {
     if (arr[i] < min) {
         min = arr[i];
     }
