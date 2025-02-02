8a0ca1963bf7c12ad9c6b830d7374fe0cacfa69a
https://github.com/MikhailV89 <mvasilchenko89@gmail.com>
2025-02-02 18:14:02 +0200

    hw4 is ready

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+const num1 = 4.67654645;
+let res = (num1 + 0.5) | 0;
+
+console.log(res);
+
+const num2 = 5.91833;
+
+let operation1 = (num2 * 100) + 0.5;
+let round = operation1 | 0;
+let result = round / 100;
+console.log(result);
