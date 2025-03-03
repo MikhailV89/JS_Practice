a37e689ad5a28aba0a96bbe43e52e45bd070dfa5
https://github.com/MikhailV89 <mvasilchenko89@gmail.com>
2025-03-03 17:27:56 +0200

    hw6 is ready

--- src/js/script.js
+++ src/js/script.js
@@ -22,26 +22,33 @@ const users = [{
         name: 'Kate',
         role: 'admin'
     }];
-let result3 = {};
-for (let i = 0; i < users.length; i++) {
-    const data = users[i].role;
-    if (result3[data] === undefined) {
-        result3[data] = 0;
-    }
-    result3[data] = (result3[data]) + 1;
-}
 
-console.log(JSON.stringify(result3).replace(/[{}"]/g, ""));
-
-let rolesString = ''
-const roles = Object.keys(result3);
-for (let i = 0; i < roles.length; i++) {
-
-     rolesString += roles[i] + ' ' + result3[i].role;
-}
-console.log(rolesString);
-
-
+// const permissionsByRoles = {
+//     admin: {
+//         home: true,
+//         dashboard: true,
+//         settings: true
+//     }
+// };
+//
+// const defaultPermissions = {
+//     home: true,
+//     dashboard: false,
+//     settings: false
+// };
+//
+// for (let i = 0; i < users.length; i ++) {
+//     // if (users[i].role === 'admin') {
+//     //     users[i].permissions = permissionsByRoles[users[i].role];
+//     // } else {
+//     //     users[i].permissions = defaultPermissions;
+//     // }
+//     console.log(users[3].role);// 'user'
+//     users[i].permissions = permissionsByRoles[users[i].role] || defaultPermissions;
+// }
+//
+// console.log(users);
+// //
 // let roleArray = [];
 //
 // for (let i = 0; i < users.length; i++) {
@@ -52,7 +59,7 @@ console.log(rolesString);
 // }
 //
 // console.log(roleArray);
-//
+// //
 // let result = {};
 //
 // for (let i = 0; i < users.length; i++) {
@@ -66,85 +73,109 @@ console.log(rolesString);
 // }
 //
 // console.log(result);
+//
 
-
-const users = [{
-    name: 'Alex',
-    role: 'admin',
-    permissions: {
-        home: true,
-        dashboard: true,
-        settings: true
+let result3 = {};
+for (let i = 0; i < users.length; i++) {
+    const data = users[i].role;
+    if (result3[data] === undefined) {
+        result3[data] = 0;
     }
-},
-    {
-        name: 'Jane',
-        role: 'admin',
-        permissions: {
-            home: true,
-            dashboard: true,
-            settings: true
-        }
-    },
-    {
-        name: 'Joe',
-        role: 'manager',
-        permissions: {
-            home: true,
-            dashboard: false,
-            settings: false
-        }
-    },
-    {
-        name: 'Bob',
-        role: 'user',
-        permissions: {
-            home: true,
-            dashboard: false,
-            settings: false
-        }
-    },
-    {
-        name: 'Lily',
-        role: 'manager',
-        permissions: {
-            home: true,
-            dashboard: false,
-            settings: false
-        }
-    },
-    {
-        name: 'Kate',
-        role: 'admin',
-        permissions: {
-            home: true,
-            dashboard: true,
-            settings: true
-        }
-    }];
-
-console.log(users[1].name); // ? Jane
-console.log(users[3].permissions['home']); // ? true
-console.log(users[3].permissions.home); // ? true
-console.log(users[3].permissions[0]); // ?
-console.log(users[3].permission.home); // ? true
-console.log(users[0].permissions.settings.home); // ?
-console.log(users[0].permissions.settings || users[1].permissions.settings); // ?
-console.log(users[0].permissions.settings && users[2].permissions.settings); // ?
-const { settings } = users[0].permissions
-console.log(settings || users[1].permissions.settings); // ?
-console.log(settings && users[2].permissions.settings); // ?
-console.log(settings - users[1].permissions.settings); // ?
-console.log(users[0].permissions.settings + users[1].permissions.settings); // ?
-
-for(let i = 1; i < users.length; i++) {
-    console.log(users[i].permissions.home); // ?
+    result3[data] = (result3[data]) + 1;
 }
 
-for(let i = 1; i < users.length; i++) {
-    console.log(users[2].permissions.settings); // ?
-}
+console.log(JSON.stringify(result3).replace(/[{}"]/g, ""));
+// // result3 = {admin: 3; manager: 2; user: 1}
+let rolesString = ''
 
-for(let i = users.length - 1; i > 0; i--) {
-    console.log(users[i].name); // ?
+const roles = Object.keys(result3);// ['user', 'manager', 'admin']
+for (let i = 0; i < roles.length; i++) {
+    const data = roles[i]
+     rolesString += data + ' - ' + result3[data] + ' ';
 }
+console.log(rolesString);
+
+
+//
+
+// const users = [{
+//     name: 'Alex',
+//     role: 'admin',
+//     permissions: {
+//         home: true,
+//         dashboard: true,
+//         settings: true
+//     }
+// },
+//     {
+//         name: 'Jane',
+//         role: 'admin',
+//         permissions: {
+//             home: true,
+//             dashboard: true,
+//             settings: true
+//         }
+//     },
+//     {
+//         name: 'Joe',
+//         role: 'manager',
+//         permissions: {
+//             home: true,
+//             dashboard: false,
+//             settings: false
+//         }
+//     },
+//     {
+//         name: 'Bob',
+//         role: 'user',
+//         permissions: {
+//             home: true,
+//             dashboard: false,
+//             settings: false
+//         }
+//     },
+//     {
+//         name: 'Lily',
+//         role: 'manager',
+//         permissions: {
+//             home: true,
+//             dashboard: false,
+//             settings: false
+//         }
+//     },
+//     {
+//         name: 'Kate',
+//         role: 'admin',
+//         permissions: {
+//             home: true,
+//             dashboard: true,
+//             settings: true
+//         }
+//     }];
+
+// console.log(users[1].name); // ? Jane
+// console.log(users[3].permissions['home']); // ? true
+// console.log(users[3].permissions.home); // ? true
+// console.log(users[3].permissions[0]); // ? undefined
+// // console.log(users[3].permission.home); // ? true// не обратил внимание что не permissions, а permission
+// console.log(users[0].permissions.settings.home); // ? undefined
+// console.log(users[0].permissions.settings || users[1].permissions.settings); // ? true первое
+// console.log(users[0].permissions.settings && users[2].permissions.settings); // ? false
+// const { settings } = users[0].permissions
+// console.log(settings || users[1].permissions.settings); // ? первое true
+// console.log(settings && users[2].permissions.settings); // ? false
+// console.log(settings - users[1].permissions.settings); // ? 0
+// console.log(users[0].permissions.settings + users[1].permissions.settings); // ? 2
+//
+//  for(let i = 1; i < users.length; i++) {
+//     console.log(users[i].permissions.home); // ? 5 true
+//  }
+// //
+// for(let i = 1; i < users.length; i++) {
+//     console.log(users[2].permissions.settings); // ? 5 false
+// }
+// //
+// for(let i = users.length - 1; i > 0; i--) {
+//     console.log(users[i].name); // ? Lily bob joe jane alex
+// }
+
